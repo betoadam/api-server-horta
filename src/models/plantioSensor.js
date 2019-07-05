9e62c49357c7f97fe3a5270546135f6abf7f994c
Marcos Vieira <mavieira866@gmail.com>
2019-07-05 09:03:28 -0300

    correcao campo plantioSensor

--- src/models/plantioSensor.js
+++ src/models/plantioSensor.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-  cultura: Number, // m2
+  cultura: String, //
   produtividade: {
     pesoBruto: Number, //kg
     pesoLiquido: Number //kg
